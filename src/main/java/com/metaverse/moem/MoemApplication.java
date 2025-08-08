ed433ee2085d9c5055736332bf1dfe6726a1fe2c
Yurim <yurim6858@g.hongik.ac.kr>
2025-08-08 16:17:00 +0900

    chore: first commit

---
+++ src/main/java/com/metaverse/moem/MoemApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.moem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metaverse/moem/MoemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.moem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

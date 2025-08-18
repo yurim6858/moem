41ff93ef1edf0c8230b053e5ad2666419dac6b98
Suwan <C289031@g.hongik.ac.kr>
2025-08-18 11:44:09 +0900

    feat: repository 추가

---
+++ src/main/java/com/metaverse/moem/team/respository/TeamRepository.java
@@ -0,0 +1,11 @@
+package com.metaverse.moem.team.respository;
+
+import com.metaverse.moem.team.domain.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// JPARepository를 상속받으면 기본적인 CRUD 메서드 자동 제공
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    // 팀 이름이 이미 존재하는지 확인하는 쿼리 메서드
+    boolean existsByName(String name);
+}

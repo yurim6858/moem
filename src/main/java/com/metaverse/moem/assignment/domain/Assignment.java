af0076f4c586dc41672a4192e1a7eeb9866c59ca
Suwan <C289031@g.hongik.ac.kr>
2025-08-19 11:37:06 +0900

    refactor: Assignment Entity UserId 추가

--- src/main/java/com/metaverse/moem/assignment/domain/Assignment.java
+++ src/main/java/com/metaverse/moem/assignment/domain/Assignment.java
@@ -21,6 +21,9 @@ public class Assignment {
     private Long projectId;
 
     @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
     private String title;
 
     private String description;

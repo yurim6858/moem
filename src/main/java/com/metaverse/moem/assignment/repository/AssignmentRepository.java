1d9a083c880600cb6bd92efe55ccf81673c23bff
Suwan <C289031@g.hongik.ac.kr>
2025-08-19 11:30:53 +0900

    feat: AssignmentRepository 추가

---
+++ src/main/java/com/metaverse/moem/assignment/repository/AssignmentRepository.java
@@ -0,0 +1,11 @@
+package com.metaverse.moem.assignment.repository;
+
+import com.metaverse.moem.assignment.domain.Assignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findByProjectId(Long projectId);
+    List<Assignment> findByProjectIdAndUserId(Long projectId, Long userId);
+}

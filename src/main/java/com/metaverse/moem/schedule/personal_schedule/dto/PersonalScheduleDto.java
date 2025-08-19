9a54065069610ee1776a4c1193ed0af3a8a4446a
Suwan <C289031@g.hongik.ac.kr>
2025-08-19 16:23:53 +0900

    feat: PersonalScheduleDto 추가

---
+++ src/main/java/com/metaverse/moem/schedule/personal_schedule/dto/PersonalScheduleDto.java
@@ -0,0 +1,22 @@
+package com.metaverse.moem.schedule.personal_schedule.dto;
+
+import java.time.LocalDateTime;
+
+public class PersonalScheduleDto {
+
+    public enum AssignmentStatus {
+        신규,
+        여유,
+        마감임박,
+        마감지남
+    }
+
+    public record Res(
+            Long id,
+            String title,
+            String description,
+            LocalDateTime dueAt,
+            boolean userCreated,
+            AssignmentStatus status
+    ) {}
+}

cdc078323721f4783b78b3b8275fceb7c605423e
Suwan <C289031@g.hongik.ac.kr>
2025-08-19 11:26:38 +0900

    feat: Assignment entity 추가

---
+++ src/main/java/com/metaverse/moem/assignment/domain/Assignment.java
@@ -0,0 +1,30 @@
+package com.metaverse.moem.assignment.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long projectId;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime dueAt;
+}

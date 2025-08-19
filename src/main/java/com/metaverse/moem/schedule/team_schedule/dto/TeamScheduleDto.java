f63b1b6c412c6f504ea1d4aa091bb0afaf503dab
Suwan <C289031@g.hongik.ac.kr>
2025-08-19 17:15:55 +0900

    feat: TeamScheduleDto 추가

---
+++ src/main/java/com/metaverse/moem/schedule/team_schedule/dto/TeamScheduleDto.java
@@ -0,0 +1,28 @@
+package com.metaverse.moem.schedule.team_schedule.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+public class TeamScheduleDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Res {
+        private Long id;
+        private String title;
+        private String description;
+        private LocalDateTime dueAt;
+        private Long assignedUserId;
+        private AssignmentStatus status;
+    }
+
+    public enum AssignmentStatus {
+        신규,
+        여유,
+        마감임박,
+        마감지남,
+        알수없음
+    }
+}

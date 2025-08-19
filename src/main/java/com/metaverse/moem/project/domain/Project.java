b2e37dc6d9b406e3608a05e8126a27dacd7b3c80
Suwan <C289031@g.hongik.ac.kr>
2025-08-19 10:27:53 +0900

    feat: Project domain, 팀 or 개인 식별용 enum 추가

---
+++ src/main/java/com/metaverse/moem/project/domain/Project.java
@@ -0,0 +1,35 @@
+package com.metaverse.moem.project.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; // project name
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProjectType type; // Team or Personal
+
+    @Column(nullable = false)
+    private Long ownerId; // team or user ID
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private boolean isDeleted;
+
+}
---
+++ src/main/java/com/metaverse/moem/project/domain/ProjectType.java
@@ -0,0 +1,6 @@
+package com.metaverse.moem.project.domain;
+
+public enum ProjectType {
+    PERSONAL,
+    TEAM
+}

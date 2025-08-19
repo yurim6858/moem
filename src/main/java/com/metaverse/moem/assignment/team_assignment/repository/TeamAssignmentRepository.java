abec0adba97a963a412d6b3c45921f9b8204e410
Suwan <C289031@g.hongik.ac.kr>
2025-08-19 12:02:37 +0900

    refactor: 팀 과제로 역할 변경

---
+++ src/main/java/com/metaverse/moem/assignment/team_assignment/repository/TeamAssignmentRepository.java
@@ -0,0 +1,11 @@
+package com.metaverse.moem.assignment.team_assignment.repository;
+
+import com.metaverse.moem.assignment.team_assignment.domain.TeamAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TeamAssignmentRepository extends JpaRepository<TeamAssignment, Long> {
+    List<TeamAssignment> findAllByProjectId(Long projectId);
+    List<TeamAssignment> findAllByProjectIdAndUserId(Long projectId, Long userId);
+}

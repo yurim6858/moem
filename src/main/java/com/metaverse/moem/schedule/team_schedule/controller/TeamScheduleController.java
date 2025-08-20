2cc42bcd34cae0cd8bf9a22ca3c6c814fae4d08e
Suwan <C289031@g.hongik.ac.kr>
2025-08-20 10:18:41 +0900

    feat: TeamScheduleController 추가

---
+++ src/main/java/com/metaverse/moem/schedule/team_schedule/controller/TeamScheduleController.java
@@ -0,0 +1,25 @@
+package com.metaverse.moem.schedule.team_schedule.controller;
+
+
+import com.metaverse.moem.schedule.team_schedule.dto.TeamScheduleDto;
+import com.metaverse.moem.schedule.team_schedule.service.TeamScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/team-schedules")
+public class TeamScheduleController {
+
+    private final TeamScheduleService teamScheduleService;
+
+    @GetMapping("/{teamId}")
+    public List<TeamScheduleDto.Res> getTeamSchedules(@PathVariable Long TeamId) {
+        return teamScheduleService.getSchedules(TeamId);
+    }
+}

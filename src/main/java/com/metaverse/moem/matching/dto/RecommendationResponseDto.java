96f70638e1e75fbd194cd624041daa094f625d36
Suwan <C289031@g.hongik.ac.kr>
2025-09-13 12:00:14 +0900

    feat: 개발용 추천을 위한 RecommendationResponseDto 추가

---
+++ src/main/java/com/metaverse/moem/matching/dto/RecommendationResponseDto.java
@@ -0,0 +1,15 @@
+package com.metaverse.moem.matching.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RecommendationResponseDto {
+    private UserDto baseUser;
+    private List<RecommendationResponseDto> recommendations;
+}

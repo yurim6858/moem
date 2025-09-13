a6cc296a88b76599400c845674f2b3430250e28f
Suwan <C289031@g.hongik.ac.kr>
2025-09-13 11:07:46 +0900

    fix : 개발용 가짜 임베딩 서비스로 연결 에러 수정

--- src/main/java/com/metaverse/moem/matching/controller/TestController.java
+++ src/main/java/com/metaverse/moem/matching/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.metaverse.moem.matching.controller;
 
 import com.metaverse.moem.matching.service.EmbeddingPort;
-import com.metaverse.moem.matching.service.OpenAIEmbeddingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
